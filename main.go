e369307496544c09b117aca960b51df409059884
Leonardo Zarkli <mr.ziqiyang@gmail.com>
2021-11-02 21:13:00 +0800

    normal

--- main.go
+++ main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 	"runtime"
 	"strings"
+	"time"
+
+	// encrypt
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 )
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+const CTRkey = "1443flfsaWfdasds"
+
+// CTR encrypt and decrypt
+func aesCtrCrypt(plainText []byte, key []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	iv := bytes.Repeat([]byte("1"), block.BlockSize())
+	stream := cipher.NewCTR(block, iv)
+	dst := make([]byte, len(plainText))
+	stream.XORKeyStream(dst, plainText)
+
+	return dst, nil
 }
 
 func system() (string, []string) {
@@ -38,6 +63,7 @@ func system() (string, []string) {
 
 func tryCmd(cmds []string) error {
 	for _, cmd := range cmds {
+		fmt.Println("[Exetuting] " + cmd)
 		splitCmd := strings.Split(cmd, " ")
 		execCmd := exec.Command(splitCmd[0], splitCmd[1:]...)
 		_, err := execCmd.CombinedOutput()
@@ -51,6 +77,19 @@ func tryCmd(cmds []string) error {
 	return nil
 }
 
+func openCompetitionPage(os string, url string) {
+	var cmds []string
+	switch os {
+	case "windows":
+		cmds = append(cmds, "cmd /c start "+url)
+	case "linux":
+		cmds = append(cmds, "xdg-open "+url)
+	case "darwin":
+		cmds = append(cmds, "open "+url)
+	}
+	tryCmd(cmds)
+}
+
 func genHosts(hosts []string) string {
 	res := ""
 	for _, host := range hosts {
@@ -59,17 +98,22 @@ func genHosts(hosts []string) string {
 	return res
 }
 
-func main() {
+func isInCompetitionTime(begin string, end string) bool {
+	// though users can change time to pass the program
 
-	var bannedHost = []string{"www.google.com", "www.google.cn", "www.google.hk", "www.google.com.hk",
-		"www.baidu.com",
-		"www.bing.com", "cn.bing.com",
-		"duckduckgo.com", "www.duckduck.com"}
-	// bannedHost := []string{"www.baidu.com"}
+	timeLocal, _ := time.LoadLocation("Asia/Shanghai")
+	startTime, _ := time.ParseInLocation("2006-01-02 15:04:05", begin, timeLocal)
+	endTime, _ := time.ParseInLocation("2006-01-02 15:04:05", end, timeLocal)
+	nowTime := time.Now()
+	if nowTime.After(startTime) && nowTime.Before(endTime) {
+		return true
+	}
+	return false
+}
 
-	hostsStr := genHosts(bannedHost)
+func add2Hosts(bannedHosts []string, hostsPath string, flushDnsCmd []string) {
 
-	hostsPath, flushDnsCmd := system()
+	hostsStr := genHosts(bannedHosts)
 
 	// write to hosts
 	filePath := hostsPath
@@ -79,8 +123,49 @@ func main() {
 	write := bufio.NewWriter(file)
 	write.WriteString(hostsStr)
 	write.Flush()
-	fmt.Println("Writing Hosts Done")
 
 	//flush DNS
 	tryCmd(flushDnsCmd)
+}
+
+func main() {
+	var bannedHosts = []string{"www.google.com", "www.google.cn", "www.google.hk", "www.google.com.hk",
+		"www.baidu.com",
+		"www.bing.com", "cn.bing.com",
+		"duckduckgo.com", "www.duckduck.com"}
+	hostsPath, flushDnsCmd := system()
+
+	curFolderPath, _ := os.Getwd()
+	tmpFilePath := path.Join(curFolderPath, "tmpFile")
+	// FIXME change the startTime and endTime into proper time
+	// NOTE startTime should be 30m before the competition start
+	if isInCompetitionTime("2021-11-02 18:00:00", "2021-11-02 23:00:00") {
+		// write encrypted original content to tmpFile
+		originHosts, _ := ioutil.ReadFile(hostsPath)
+		originHosts, _ = aesCtrCrypt(originHosts, []byte(CTRkey))
+		err := ioutil.WriteFile(tmpFilePath, originHosts, 0666)
+		check(err)
+		// write to hosts
+		add2Hosts(bannedHosts, hostsPath, flushDnsCmd)
+
+		fmt.Println("All Done!")
+		competitionUrl := "https://dict.youdao.com/search?q=is%20opening&keyfrom=new-fanyi.smartResult"
+		// exec.Command("xdg-open", competitionUrl).Run()
+		// openCompetitionPage(runtime.GOOS, competitionUrl)
+		fmt.Println("The competiton page is opening...")
+		fmt.Println("if it goes wrong, please visit the website " + competitionUrl)
+		fmt.Println("[Please don't delete tmpfile in the current directory]")
+		fmt.Println("[Please reexecute this program after the competition to ensure your internet connection is normal]")
+	} else {
+		// read and
+		encrypted, e := ioutil.ReadFile(tmpFilePath)
+		check(e)
+		originHosts, _ := aesCtrCrypt(encrypted, []byte(CTRkey))
+		err := ioutil.WriteFile(hostsPath, originHosts, 0666)
+		check(err)
+		fmt.Println("[ALL have been restored to original state]")
+		fmt.Println("Appreciation for yout attendance and Congratulations for your completion!")
+	}
+	fmt.Println("[Press ENTER key to exit...]")
+	fmt.Scanln()
 }
